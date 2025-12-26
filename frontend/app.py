ccaecee0c0c341bbd4ae2361906a216be1777ff8
Németh Dávid <ho92ms@std.uni-eszterhazy.hu>
2025-12-26 22:31:15 +0100

    Streamlit Cloud secrets st.secrets alapu eleresenek implamentalasa

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,10 @@ import requests
 import streamlit as st
 import matplotlib.pyplot as plt
 
-API_BASE = os.getenv("BACKEND_BASE_URL", "http://127.0.0.1:8000")
+
+API_BASE = st.secrets.get("BACKEND_BASE_URL", os.getenv("BACKEND_BASE_URL", "http://127.0.0.1:8000"))
+
+#API_BASE = os.getenv("BACKEND_BASE_URL", "http://127.0.0.1:8000")
 
 st.set_page_config(page_title="Travel Wishlist", layout="wide")
 st.title("Travel Wishlist")
